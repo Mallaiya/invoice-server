999c26322f485c6aa33535a0555de2fd9fdeecb9
Mallaiya <mallaiyazap@gmail.com>
2019-06-18 12:19:03 +0530

    PDF Parser modification

--- routes/Users.js
+++ routes/Users.js
@@ -141,8 +141,17 @@ users.get('/fetch-pdf', (req, res) => {
 users.post('/invoice-add', (req, res) => {
   let currentDate = new Date();
   let date = currentDate.getDate();
+  if(date < 10){
+    date = '0' + date;
+  }
   let month = currentDate.getMonth(); 
+  if(month < 10){
+   month = '0' + month;
+  }
   let year = currentDate.getFullYear();
+  if(year < 10) {
+    year = '0' + year;
+  }
   let hour = currentDate.getHours().toString();
   if(hour<10){
     hour = '0' + hour;
@@ -224,9 +233,42 @@ users.post('/invoice-remove', (req, res) => {
 })
 
 
-users.post('/send-mail', (req, res) => {
-  const output = `
-    <p>${req.body.message}</p>
-  `
-})
+// users.post('/send-mail', (req, res) => {
+//   const output =  `
+//     <p>Hi I am checking mail</p>
+//   `;
+//   let transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: 'sylvia79@ethereal.email', // generated ethereal user
+//       pass: 'U9STqvQzt45vGsRPxE' // generated ethereal password
+//     }
+//   });
+
+//   // send mail with defined transport object
+//   let mailOptions = {
+//     from: '"Nodemailer Contact" <sylvia79@ethereal.email>', // sender address
+//     to: "mallaiyazap@gmail.com", // list of receivers
+//     subject: "Node Mail", // Subject line
+//     text: "Checking node mail", // plain text body
+//     html: output // html body
+//   };
+  
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if(error) {
+//       return console.log(error);
+//     }
+  
+
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   })
+// })
+
+
 module.exports = users
